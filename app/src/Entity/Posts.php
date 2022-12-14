b03dea4f9dc0af867e2cba6ac3f11048854e8a01
kudria <herman.kudria@primesoft.pl>
2022-12-14 12:58:54 +0100

    feat: add migration and entity

---
+++ app/src/Entity/Posts.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostsRepository::class)]
+class Posts
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $postId = null;
+
+    #[ORM\Column]
+    private ?int $userId = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $userName = null;
+
+    #[ORM\Column(length: 500)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 2000)]
+    private ?string $body = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPostId(): ?int
+    {
+        return $this->postId;
+    }
+
+    public function setPostId(int $postId): self
+    {
+        $this->postId = $postId;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->userName;
+    }
+
+    public function setUserName(string $userName): self
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+}
